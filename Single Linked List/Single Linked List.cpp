b8acbad3b3f2aee04e9cd38af26d5bb466b2e455
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-19 10:51:32 +0700

    aa

--- Single Linked List/Single Linked List.cpp
+++ Single Linked List/Single Linked List.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 using namespace std;
 
+struct Node
+{
+	int noMhs;
+	string name;
+	Node* next;
+};
+
+Node* START = NULL;
+
+void addNote() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+
+	cout << "Masukkan NIM : ";
+	cin >> nim;
+	cout << "Masukkan Nama : ";
+	cin >> nama;
+
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START -> )
+}
